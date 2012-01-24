6bd368ea03672c3c558d19f28adc480f6dfbd8dc
Nikita Chudakov <adsenceg@ya.ru>
2012-01-24 22:32:39 +0400

    build classpath fix

--- src/com/musicbox/server/DiscardServerHandler.java
+++ src/com/musicbox/server/DiscardServerHandler.java
@@ -1,18 +1,24 @@
 package com.musicbox.server;
 
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.*;
 
 public class DiscardServerHandler extends SimpleChannelHandler {
-
     @Override
-    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) {
+        Channel ch = e.getChannel();
+        
+        ChannelBuffer time = ChannelBuffers.buffer(4);
+        time.writeInt((int) (System.currentTimeMillis() / 1000));
+        
+        ChannelFuture f = ch.write(time);
+        
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
         e.getCause().printStackTrace();
-        
-        Channel ch = e.getChannel();
-        ch.close();
+        e.getChannel().close();
     }
 }
