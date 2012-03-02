1aded4124f13d9f82c73f1ece5e6fb302dee5f2f
giko <giko@giko-Aspire-4830TG.(none)>
2012-03-02 17:21:32 +0400

    Visualization begin

--- content/js/player.js
+++ content/js/player.js
@@ -4,12 +4,14 @@
 //@email: yensamg@gmail.com
 //@license: Feel free to use it, but keep this credits please!
 /***************************/
-
+var canvas;
+var ctx;
 //This is the class that interact with the interface
 var player = new (function () {
     soundManager.url = '/bootstrap/swf';
     soundManager.flashVersion = 9; // optional: shiny features (default = 8)
     soundManager.useFlashBlock = false; // optionally, enable when you're ready to dive in
+    soundManager.useWaveformData = true;
     var sound;
     //The song position
     this.position = -1;
@@ -17,6 +19,7 @@ var player = new (function () {
     this.volume = 3;
     //Status: 0:pause, 1:play
     this.status = 1;
+    this.timeout = 30;
 
     this.nextSong = function () {
         if (this.position + 1 == this.playList.length)
@@ -31,6 +34,16 @@ var player = new (function () {
             }).fadeIn();
     }
     this.play = function (audio) {
+        canvas = document.getElementById("example");
+        canvas.height = 480;
+
+        canvas.width = 640;
+        ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        for (var i = 0; i < 256; i++) {
+            ctx.lineTo(i * 10, i * Math.cos(i) + 50);
+        }
+        ctx.stroke();
         this.status = 0;
         $("#play").css("backgroundImage", "url('../images/pause.jpg')");
         if (typeof(sound) != 'undefined') {
@@ -39,8 +52,19 @@ var player = new (function () {
         sound = soundManager.createSound({
             id:'mySound', // required
             url:audio.url, // required
-            autoPlay:true
+            autoPlay:true,
+            whileplaying:function () {
+                if (player.timeout <= 0) {
+                    $(".position").text(Math.round(sound.position / 1000 / 60) + '/' + Math.round(sound.duration / 1000 / 60));
+                    console.log(sound.peakData.right);
+                    player.timeout = 30;
+                } else {
+                    player.timeout--;
+                }
+
+            }
         });
+
         this.setTitle(audio.artist + ' - ' + audio.title);
     }
     //Jump to he previous or last song if it is in the first position
@@ -67,6 +91,7 @@ var player = new (function () {
     this.volumeInc = function () {
         if (this.volume + 1 <= 3) {
             this.volume++;
+            sound.setVolume(33 * this.volume);
             var me = this;
             $("#volume").fadeOut(200,
                 function () {
@@ -78,6 +103,7 @@ var player = new (function () {
     this.volumeDec = function () {
         if (this.volume - 1 > 0) {
             this.volume--;
+            sound.setVolume(33 * this.volume);
             var me = this;
             $("#volume").fadeOut(200,
                 function () {
@@ -105,3 +131,29 @@ var player = new (function () {
         }
     }
 });
+
+//Captures the key press events
+document.onkeydown = function (e) {
+    var ev = e;
+    if (ev.charCode && ev.charCode == 32)
+        player.playPause();
+    else {
+        switch (ev.keyCode) {
+            case 32:
+                player.playPause();
+                break;
+            case 39:
+                player.nextSong();
+                break;
+            case 37:
+                player.prevSong();
+                break;
+            case 38:
+                player.volumeInc();
+                break;
+            case 40:
+                player.volumeDec();
+                break;
+        }
+    }
+}
